d8287fd5c57568187be551fb3a5116b2d15ac6e5
yRaposo <ynyght@gmail.com>
2025-11-08 14:47:57 -0300

    feat: remove frontend code and add backend structure
    - Deleted the frontend code including package.json, StylizedInput component, and LoginScreen.
    - Added backend structure with Maven configuration including pom.xml, application properties, and main application class.
    - Included necessary dependencies for Spring Boot, JPA, and security in the pom.xml.
    - Created Maven wrapper scripts for easier project setup.

---
+++ Fatec_Meets_BackEnd/meetsbackend/src/main/java/com/meets/meetsbackend/MeetsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.meets.meetsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetsbackendApplication.class, args);
+	}
+
+}
---
+++ Fatec_Meets_BackEnd/meetsbackend/src/test/java/com/meets/meetsbackend/MeetsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meets.meetsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
